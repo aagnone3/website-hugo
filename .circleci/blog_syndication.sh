80a9eb291a66846963d303d58dbab2abfa75396e
aagnone <anthonyagnone@gmail.com>
2019-08-09 15:47:16 -0400

    update

--- .circleci/blog_syndication.sh
+++ .circleci/blog_syndication.sh
@@ -11,7 +11,7 @@ function master_update() {
     # NOTE: moving to commit t-1 to obtain diffs assumes a consistent squash-and-merge scheme, which
     # therefore only creates one commit per pull request
     git checkout -b temp
-    git reset --hard $(git log | grep "^commit [a-zA-Z0-9]*$" | head -n1 | cut -d ' ' -f2)
+    git reset --hard $(git log | grep "^commit [a-zA-Z0-9]*$" | tail -n+2 | head -n1 | cut -d ' ' -f2)
     diff_output=$(git diff --diff-filter=D ${CIRCLE_BRANCH}..)
 }
 
@@ -28,6 +28,8 @@ function pull_request() {
 
 # use a diff to detect and syndicate any new blog posts
 [[ ! -z ${diff_output} ]] && {
+    echo "Looking at diff output"
+
     # dump blog-specific file additions to a file for reading
     git diff --diff-filter=A ..${CIRCLE_BRANCH} | grep 'diff.*content\/.*index\.md' > /tmp/new_files.lst
     n_new_blogs=$(cat /tmp/new_files.lst | wc -l)
@@ -40,7 +42,6 @@ function pull_request() {
     [[ ${n_new_blogs} -gt 0 ]] && {
         while read line; do
             name=$(echo ${line} | cut -d ' ' -f4 | rev | cut -d/ -f2 | rev)
-            echo $name > log.txt
             echo $name
             # TODO pass extra flag to indicate verify vs deploy
             python syndication/src/syndicate.py content/post/${name}/index.md
@@ -48,5 +49,6 @@ function pull_request() {
     }
 } || {
     # no new blogs, still need to return to active branch
+    echo "No diff output"
     git checkout ${CIRCLE_BRANCH}
 }
