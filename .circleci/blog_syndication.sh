7e155a795ccf0939906f455757c16ae7099888a5
aagnone <anthonyagnone@gmail.com>
2019-08-08 20:14:27 -0400

    update

--- .circleci/blog_syndication.sh
+++ .circleci/blog_syndication.sh
@@ -11,7 +11,7 @@ diff_output=$(git diff --diff-filter=D ${CIRCLE_BRANCH}..)
     git diff --diff-filter=A ..${CIRCLE_BRANCH} | grep 'diff.*content\/.*index\.md' > /tmp/new_files.lst
     while read line; do
         name=$(echo ${line} | cut -d ' ' -f4 | rev | cut -d/ -f2 | rev)
-        echo $name >> names.lst
-        python syndication/src/syndicate.py content/post/${name}/index.md > log.$line.txt
+        echo $name >> log.txt
+        python syndication/src/syndicate.py content/post/${name}/index.md >> log.txt
     done < /tmp/new_files.lst
 }
