ceca6c7a39c8769d55389ab6e7acc20294f2a088
aagnone <anthonyagnone@gmail.com>
2019-08-08 20:30:15 -0400

    update

--- .circleci/blog_syndication.sh
+++ .circleci/blog_syndication.sh
@@ -3,11 +3,17 @@ set -eou pipefail
 
 # TODO obtain target branch from github curl
 target_branch=master
+
+# reset target branch to its status in origin to create the current diff
 git checkout ${target_branch}
 git reset --hard origin/${target_branch}
 diff_output=$(git diff --diff-filter=D ${CIRCLE_BRANCH}..)
 
+# use a diff to detect and syndicate any new blog posts
 [[ ! -z ${diff_output} ]] && {
+    # return to the active branch to access new content being merged
+    git checkout ${CIRCLE_BRANCH}
+
     git diff --diff-filter=A ..${CIRCLE_BRANCH} | grep 'diff.*content\/.*index\.md' > /tmp/new_files.lst
     while read line; do
         name=$(echo ${line} | cut -d ' ' -f4 | rev | cut -d/ -f2 | rev)
